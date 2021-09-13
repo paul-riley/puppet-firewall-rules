e426d02bbc75246cb39c6c3576fa5991a28239e3
Paul Riley <paul.riley@puppet.com>
2021-09-13 13:05:03 -0700

    puppet firewall rules

---
+++ firewall-rules.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#From the Firewalld Cheatsheet: https://www.liquidweb.com/kb/an-introduction-to-firewalld/
+
+firewall-cmd --permanent --add-port=443/TCP
+firewall-cmd --permanent --add-port=8081/TCP
+firewall-cmd --permanent --add-port=4433/TCP
+firewall-cmd --permanent --add-port=8170/TCP
+firewall-cmd --permanent --add-port=8140/TCP
+firewall-cmd --permanent --add-port=8143/TCP
