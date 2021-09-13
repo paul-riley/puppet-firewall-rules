8ed5dcf94ab6fd0dfde8d01ff2137bfce5471043
Paul Riley <paul.riley@puppet.com>
2021-09-13 13:15:50 -0700

    updated so the redirect works

--- firewall-rules.sh
+++ firewall-rules.sh
@@ -2,6 +2,7 @@
 
 #From the Firewalld Cheatsheet: https://www.liquidweb.com/kb/an-introduction-to-firewalld/
 
+firewall-cmd --permanent --add-port=80/TCP
 firewall-cmd --permanent --add-port=443/TCP
 firewall-cmd --permanent --add-port=8081/TCP
 firewall-cmd --permanent --add-port=4433/TCP
