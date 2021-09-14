18e22207de1edc90adbd5c0214c365626fb256bf
Paul Riley <paul.riley@puppet.com>
2021-09-14 11:43:01 -0700

    added rule, order, put note in

--- firewall-rules.sh
+++ firewall-rules.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-#From the Firewalld Cheatsheet: https://www.liquidweb.com/kb/an-introduction-to-firewalld/
+#Make this executable: chmod +x
 
 firewall-cmd --permanent --add-port=80/tcp
 firewall-cmd --permanent --add-port=443/tcp
 firewall-cmd --permanent --add-port=8081/tcp
 firewall-cmd --permanent --add-port=4433/tcp
-firewall-cmd --permanent --add-port=8170/tcp
 firewall-cmd --permanent --add-port=8140/tcp
+firewall-cmd --permanent --add-port=8142/tcp
 firewall-cmd --permanent --add-port=8143/tcp
+firewall-cmd --permanent --add-port=8170/tcp
+firewall-cmd --reload
