1541210f8f0317d0cf136f7b78977d456c9f9a3e
Paul Riley <paul.riley@puppet.com>
2021-09-14 11:36:49 -0700

    tcp must be lower case

--- firewall-rules.sh
+++ firewall-rules.sh
@@ -2,11 +2,10 @@
 
 #From the Firewalld Cheatsheet: https://www.liquidweb.com/kb/an-introduction-to-firewalld/
 
-firewall-cmd --permanent --add-port=80/TCP
-firewall-cmd --permanent --add-port=443/TCP
-firewall-cmd --permanent --add-port=8081/TCP
-firewall-cmd --permanent --add-port=4433/TCP
-firewall-cmd --permanent --add-port=8170/TCP
-firewall-cmd --permanent --add-port=8140/TCP
-firewall-cmd --permanent --add-port=8143/TCP
-
+firewall-cmd --permanent --add-port=80/tcp
+firewall-cmd --permanent --add-port=443/tcp
+firewall-cmd --permanent --add-port=8081/tcp
+firewall-cmd --permanent --add-port=4433/tcp
+firewall-cmd --permanent --add-port=8170/tcp
+firewall-cmd --permanent --add-port=8140/tcp
+firewall-cmd --permanent --add-port=8143/tcp
